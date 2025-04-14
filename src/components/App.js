fe72b7d8f4482c8cc3eaa3932e082428feca68fa
Faith <kashfaith77@gmail.com>
2025-04-14 18:16:20 +0300

    Fetch data from a db in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+//create app.js
+import React,{useState,useEffect} from "react";
+function App(){
+    const [image,setImage]=useState([])
+    const [isloaded, setIsLoaded]=useState(false)
+    useEffect(()=>{
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then((data)=>{
+            console.log(data)
+            setImage(data)
+            setIsLoaded(true)
+        })
+        
+    },[])
+    if(!isloaded){
+        return <p>Loading...</p>
+    }
+    return(
+        <img src={image.message} alt="A Random Dog"/>
+       
+    )
+}
+export default App
